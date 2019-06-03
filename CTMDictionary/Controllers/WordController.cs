ef02216077b382fed928b4f181223a8a5d7c5b6c
Rob Harris <robert.harris@bglgroup.co.uk>
2019-06-03 13:30:01 +0100

    Initial Commit. Api with a single endpoint for helloworld

---
+++ CTMDictionary/Controllers/WordController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CTMDictionary.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WordController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "hello world";
+        }
+    }
+}
